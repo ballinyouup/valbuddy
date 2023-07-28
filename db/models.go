47175b5ea56dce3ede98c4241505583013257880
ballinyouup <bryanhuici@gmail.com>
2023-07-28 08:07:02 -0400

    added provider and moved fields

--- db/models.go
+++ db/models.go
@@ -5,12 +5,12 @@ import (
 )
 
 type User struct {
-	UserID    string    `json:"user_id" gorm:"unique"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	UserID    string    `json:"user_id" gorm:"primaryKey"`
 	Email     string    `json:"email" gorm:"unique"`
 	Username  string    `json:"username" gorm:"unique"`
 	Role      string    `json:"role"`
 	Image     string    `json:"image_url"`
+	Provider  string	`json:"provider"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
-
