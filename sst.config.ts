4a8e47ccd245763c302098caf0baab94ca12ece5
ballinyouup <bryanhuici@gmail.com>
2023-07-31 22:37:46 -0400

    installed sst

---
+++ sst.config.ts
@@ -0,0 +1,16 @@
+import type { SSTConfig } from "sst";
+import Site from "./stacks/site";
+import Backend from "./stacks/backend";
+
+export default {
+  config(_input) {
+    return {
+      name: "sveltekit-go",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(Site);
+    app.stack(Backend);
+  },
+} satisfies SSTConfig;
