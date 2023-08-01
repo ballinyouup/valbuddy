cef724bd936e8e1a8c3fa277fa2dc8b6799a8afb
ballinyouup <bryanhuici@gmail.com>
2023-07-31 22:37:20 -0400

    added login with discord link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	
+</script>
+
+<a href="https://eap6uv9is7.execute-api.us-east-1.amazonaws.com/production/login/discord">Discord</a>
