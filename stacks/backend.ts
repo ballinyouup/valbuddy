bbc7da897f56d654abde8e767a77bdba4ba7e819
ballinyouup <bryanhuici@gmail.com>
2023-08-02 16:23:09 -0400

    added env variables in sst

--- stacks/backend.ts
+++ stacks/backend.ts
@@ -5,6 +5,12 @@ export default function Backend({ stack }) {
     const lambdaFunc = new Function(stack, "goLambda", {
         handler: "./api/main.go",
         runtime: "go1.x",
+        environment: {
+            DISCORD_ID: process.env.DISCORD_ID as string,
+            DISCORD_SECRET: process.env.DISCORD_SECRET as string,
+            DATABASE_URL: process.env.DATABASE_URL as string,
+            API_URL: process.env.API_URL as string
+        }
     })
     new ApiGatewayV1Api(stack, "goApi", {
         routes: {
