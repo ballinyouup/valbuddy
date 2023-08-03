b821227270af3c3eb575d12c887b32b1f51b8cb4
ballinyouup <bryanhuici@gmail.com>
2023-08-02 22:04:21 -0400

    added validate to user struct

--- api/db/models.go
+++ api/db/models.go
@@ -5,12 +5,12 @@ import (
 )
 
 type User struct {
-	UserID    string    `json:"user_id" gorm:"primaryKey"`
-	Email     string    `json:"email" gorm:"unique"`
-	Username  string    `json:"username" gorm:"unique"`
-	Role      string    `json:"role"`
-	Image     string    `json:"image_url"`
-	Provider  string	`json:"provider"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	UserID    string    `json:"user_id" gorm:"primaryKey" validate:"required,min=1"`
+	Email     string    `json:"email" gorm:"unique" validate:"required,email"`
+	Username  string    `json:"username" gorm:"unique" validate:"required,min=1"`
+	Role      string    `json:"role" validate:"required,min=1"`
+	Image     string    `json:"image_url" validate:"required,min=1"`
+	Provider  string    `json:"provider" validate:"required,min=1"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
