43a680d1dd988c2db295617217b1668f3ee9ce66
ballinyouup <bryanhuici@gmail.com>
2023-08-09 07:15:35 -0400

    added protected route example

--- api/routes/home.go
+++ api/routes/home.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"fmt"
+	"nextjs-go/db"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -9,7 +12,19 @@ func Home(app *fiber.App) {
 		return c.SendString("Welcome!")
 	})
 	app.Get("/hello", func(c *fiber.Ctx) error {
-		return c.SendString("Hello!")
+		s, err := db.Sessions.Get(c)
+		if err != nil {
+			return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("/hello > %s", err))
+		}
+		if !s.Fresh() {
+			userId := s.Get("user_id")
+			user := db.User{}
+			db.Database.Find(&user, "user_id = ?", userId)
+			return c.SendString(fmt.Sprintf("Welcome %s", user.Username))
+		} else {
+			s.Destroy()
+			return fiber.NewError(fiber.StatusUnauthorized, "Unauthorized")
+		}
 	})
 	app.Get("/test", func(c *fiber.Ctx) error {
 		return c.SendString("test!")
