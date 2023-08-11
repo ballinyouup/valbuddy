68eaf93d3380ebcee3c4bb12eb8746d3a739e2c5
ballinyouup <bryanhuici@gmail.com>
2023-08-11 04:12:55 -0400

    added create account component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import { config } from "@/env";
+import { CreateAccount } from "@/components/create-account";
 export default function Login() {
 	return (
 		<div className="flex flex-col gap-4">
 			<Link href={`${config.API_URL}/login/discord`}>Discord</Link>
 			<Link href={`${config.API_URL}/login/twitch`}>Twitch</Link>
+			<CreateAccount />
 		</div>
 	);
 }
