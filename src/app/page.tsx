530823759dbd980a4c3da80f6a52ff9b960d2bc5
ballinyouup <bryanhuici@gmail.com>
2023-08-11 04:39:44 -0400

    check cookie for session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 import { cookies } from "next/headers";
 import { config } from "@/env";
 export default async function Home() {
-	if (cookies().toString() === "") {
+	const session = cookies().get("session_id")
+	if (session === undefined) {
 		return <Link href="/login">Sign In</Link>;
 	}
 	const data = await fetch(`${config.API_URL}/hello`, {
 		credentials: "include",
 		method: "GET",
-		headers: { Cookie: cookies().toString() },
+		headers: { Cookie:  cookies().toString()},
 	});
 	const res = (await data.json()) as { message: string };
 	return <main className="flex flex-col">{"Server: " + res.message}</main>;
