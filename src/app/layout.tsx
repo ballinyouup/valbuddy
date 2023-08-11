83a90ff1c61830c9f225e65e45f9f01b0c0c697b
ballinyouup <bryanhuici@gmail.com>
2023-08-11 10:37:58 -0400

    added toast, bug if i put toaster under theme prov

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,8 +18,10 @@ export default function RootLayout({
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body className={inter.className}>
+			<Toaster />
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 					{children}
+					
 				</ThemeProvider>
 			</body>
 		</html>
