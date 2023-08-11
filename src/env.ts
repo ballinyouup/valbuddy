badb11cd2c19b7e1ad314b2341e24c6aa975defe
ballinyouup <bryanhuici@gmail.com>
2023-08-11 06:37:24 -0400

    changed local url to new rewrite url

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
 export const config = {
-    API_URL: process.env.NODE_ENV === "development" ? "http://127.0.0.1:3001" : process.env.API_URL as string,
+    API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : process.env.API_URL as string,
 }
