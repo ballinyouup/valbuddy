4d2fb1c572e91f72d12198fd4259d08d3db3f974
ballinyouup <bryanhuici@gmail.com>
2023-08-11 11:02:34 -0400

    added api url env to frontend

--- stacks/site.ts
+++ stacks/site.ts
@@ -1,7 +1,11 @@
 import { NextjsSite } from "sst/constructs";
 
 export default function Site({ stack }: any) {
-    const site = new NextjsSite(stack, "site");
+    const site = new NextjsSite(stack, "site", {
+        environment: {
+            API_URL: process.env.API_URL as string,
+        }
+    });
     stack.addOutputs({
         url: site.url,
     });
