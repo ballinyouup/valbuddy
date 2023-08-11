c1e744e256021f5b9f3f247b0501af7fbe693073
ballinyouup <bryanhuici@gmail.com>
2023-08-11 12:09:39 -0400

    added api url

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
 export const config = {
-    API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : process.env.API_URL as string,
+    API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : "https://api.valbuddy.com",
 }
