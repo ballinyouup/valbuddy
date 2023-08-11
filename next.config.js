05e1d8c6fdf7b140b54a9b12d11bb93463cc4ad9
ballinyouup <bryanhuici@gmail.com>
2023-08-11 06:25:27 -0400

    added rewrite for local dev

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async rewrites() {
+        if(process.env.NODE_ENV === "development"){
+            return [
+                {
+                    source: "/api/:path*",
+                    destination: "http://127.0.0.1:3001/:path*",
+                },
+            ];
+        }
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
