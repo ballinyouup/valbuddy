901e5be2cc9729d9fc02f13925e17758d6db9998
ballinyouup <bryanhuici@gmail.com>
2023-08-11 04:12:42 -0400

    added auth cookie check to prevent app crash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 import { cookies } from "next/headers";
 import { config } from "@/env";
 export default async function Home() {
+	if (cookies().toString() === "") {
+		return <Link href="/login">Sign In</Link>;
+	}
 	const data = await fetch(`${config.API_URL}/hello`, {
 		credentials: "include",
 		method: "GET",
 		headers: { Cookie: cookies().toString() },
 	});
-	if (!data.ok) {
-		return <Link href="/login">Sign In</Link>;
-	}
 	const res = (await data.json()) as { message: string };
 	return <main className="flex flex-col">{"Server: " + res.message}</main>;
 }
