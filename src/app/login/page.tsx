a36a2e77c135954172e4ee7e9f4022fd9cbd1f11
ballinyouup <bryanhuici@gmail.com>
2023-08-11 10:38:22 -0400

    centered page and removed links

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,8 @@
-import Link from "next/link";
-import { config } from "@/env";
-import { CreateAccount } from "@/components/create-account";
+import CreateAccount from "@/components/create-account";
+
 export default function Login() {
 	return (
-		<div className="flex flex-col gap-4">
-			<Link href={`${config.API_URL}/login/discord`}>Discord</Link>
-			<Link href={`${config.API_URL}/login/twitch`}>Twitch</Link>
+		<div className="flex flex-col gap-4 items-center justify-center h-full">
 			<CreateAccount />
 		</div>
 	);
