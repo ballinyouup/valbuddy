3ce5398e97e48cd0a86d5072fa7f51725cca76c5
ballinyouup <bryanhuici@gmail.com>
2023-08-11 12:59:02 -0400

    toast mount into its own client component

---
+++ src/components/sign-in-toast.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useMounted } from "@/hooks/use-mounted";
+import { useToast } from "@/components/ui/use-toast";
+import { useEffect } from "react";
+
+export default function SignInToast({error}: {error: string | string[] | undefined}) {
+	const { toast } = useToast();
+	const mount = useMounted()
+	useEffect(() => {
+		if(error && typeof error === "string" && mount){
+			toast({
+				title: error,
+				description: "Please Log in with Another Provider",
+				variant: "destructive"
+			});
+		}
+	}, [error, toast, mount]);
+	return <></>
+}
