fe56cada803a28343570ed8d3a0d5fff678c1077
ballinyouup <bryanhuici@gmail.com>
2023-08-15 16:36:50 -0400

    added client api url

--- stacks/site.ts
+++ stacks/site.ts
@@ -1,7 +1,11 @@
 import { NextjsSite } from "sst/constructs";
 
 export default function Site({ stack }: any) {
-    const site = new NextjsSite(stack, "site");
+    const site = new NextjsSite(stack, "site", {
+        environment: {
+            NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL as string
+        }
+    });
     stack.addOutputs({
         url: site.url,
     });
