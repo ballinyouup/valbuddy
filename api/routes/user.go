7ec4e951b2d249d53b9171aa69886579ad7d75e3
ballinyouup <bryanhuici@gmail.com>
2023-08-15 18:42:56 -0400

    renamed to user

---
+++ api/routes/user.go
@@ -0,0 +1,26 @@
+package routes
+
+import (
+	"fmt"
+	"nextjs-go/db"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func User(app *fiber.App) {
+	app.Get("/user", func(c *fiber.Ctx) error {
+		s, err := db.Sessions.Get(c)
+		if err != nil {
+			return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("/user > %s", err))
+		}
+		if !s.Fresh() {
+			userId := s.Get("user_id")
+			user := db.User{}
+			db.Database.Where("user_id = ?", userId).First(&user)
+			return c.JSON(user)
+		} else {
+			s.Destroy()
+			return fiber.NewError(fiber.StatusUnauthorized, "Unauthorized")
+		}
+	})
+}
