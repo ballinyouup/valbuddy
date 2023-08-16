46d160331ca0a54a869bea4de00ff94649fafb01
ballinyouup <bryanhuici@gmail.com>
2023-08-16 19:41:58 -0400

    added select to db query

--- api/handlers/user.go
+++ api/handlers/user.go
@@ -24,7 +24,7 @@ func User(c *fiber.Ctx) error {
 		user := db.User{}
 
 		// Retrieve user data from the database based on the user ID
-		db.Database.Where("id = ?", userId).First(&user)
+		db.Database.Select("id", "image", "username").Where("id = ?", userId).First(&user)
 
 		// Return the user data as JSON response
 		return c.JSON(user)
