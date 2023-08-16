83b9d690ebd07cf863029520f5b9654c1800d0ac
ballinyouup <bryanhuici@gmail.com>
2023-08-16 14:12:55 -0400

    logout button

---
+++ src/components/logout.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { config } from "@/env";
+import { Button } from "./ui/button";
+import { useRouter} from "next/navigation";
+
+export default function LogoutButton({ children }: { children?: React.ReactNode; }) {
+    const router = useRouter()
+    async function Logout(){
+        const data = await fetch(`${config.API_URL}/logout`, {
+            credentials: "include",
+            method: "GET",
+        });
+        if (data.ok) {
+            router.refresh()
+        }
+    }
+    
+    return <Button onClick={() => Logout()} className="w-full">{children}</Button>;
+}
