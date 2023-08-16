f3b7b6bcff9cb9c4d97ae94df87e1cda6c0c3656
ballinyouup <bryanhuici@gmail.com>
2023-08-16 17:24:30 -0400

    renamed to id for consist, new acc and posts

--- api/db/models.go
+++ api/db/models.go
@@ -5,7 +5,7 @@ import (
 )
 
 type User struct {
-	UserID    string    `json:"user_id" gorm:"primaryKey" validate:"required,min=1"`
+	ID        string    `json:"id" gorm:"primaryKey" validate:"required,min=1"`
 	Email     string    `json:"email" gorm:"unique" validate:"required,email"`
 	Username  string    `json:"username" gorm:"unique" validate:"required,min=1"`
 	Role      string    `json:"role" validate:"required,min=1"`
@@ -13,4 +13,19 @@ type User struct {
 	Provider  string    `json:"provider" validate:"required,min=1"`
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	
+}
+
+type Account struct {
+	ID       string `json:"id" gorm:"primaryKey" validate:"required,min=1"`
+	UserID   string `json:"user_id" validate:"required,min=1"`
+	Username string `json:"username" validate:"omitempty"`
+	Rank     string `json:"rank" validate:"omitempty"`
+
+}
+
+type Post struct {
+	ID     string `json:"id" gorm:"primaryKey" validate:"required,min=1"`
+	UserID string `json:"user_id" validate:"required,min=1"`
+	Text   string `json:"text" validate:"required,min=1"`
 }
