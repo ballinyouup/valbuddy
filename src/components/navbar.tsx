ed133f3be00a32f1b75a45b012acb67015988a1e
ballinyouup <bryanhuici@gmail.com>
2023-08-16 12:30:06 -0400

    refactored out fetch

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,51 +1,29 @@
 import Link from "next/link";
-import { cookies } from "next/headers";
-import { config } from "@/env";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
-interface User {
-	user_id: string;
-	email: string;
-	username: string;
-	role: string;
-	image_url: string;
-	provider: string;
-	created_at: Date;
-	updated_at: Date;
-}
+import { GetUser } from "../api/user";
 
 export default async function Navbar() {
-	return (
-		<nav className="flex flex-row h-fit p-4 bg-secondary w-full justify-between">
+    return (
+        <nav className="flex flex-row h-fit p-4 bg-secondary w-full justify-between">
             <h4>V</h4>
-			<NavProfile />
-		</nav>
-	);
+            <NavProfile />
+        </nav>
+    );
 }
 
 async function NavProfile() {
-	const session = cookies().get("session_id");
-	if (session === undefined) {
-		return (
-			<Link href="/login">
-				<Button>Sign In</Button>
-			</Link>
-		);
-	}
-	const data = await fetch(`${config.API_URL}/user`, {
-		credentials: "include",
-		method: "GET",
-		headers: { Cookie: cookies().toString() },
-		cache: "force-cache",
-		next: {
-			tags: ["user"],
-		},
-	});
-	const user = (await data.json()) as User;
-	return (
-		<Avatar>
-			<AvatarImage src={user.image_url} alt="@shadcn" />
-			<AvatarFallback>{user.username[0]}</AvatarFallback>
-		</Avatar>
-	);
+    const user = await GetUser();
+    if (!user) {
+        return <Link href="/login">
+            <Button>Sign In</Button>
+        </Link>;
+    }
+    return (
+        <Avatar>
+            <AvatarImage src={user.image_url} alt="@shadcn" />
+            <AvatarFallback>{user.username[0]}</AvatarFallback>
+        </Avatar>
+    );
 }
+
