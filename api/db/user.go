13368a10b598d7b049cc5c16e8135c4e5ac55f28
ballinyouup <bryanhuici@gmail.com>
2023-08-16 22:53:39 -0400

    moved validate check fn out to utils

--- api/db/user.go
+++ api/db/user.go
@@ -2,9 +2,6 @@ package db
 
 import (
 	"fmt"
-	"strings"
-
-	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"github.com/lucsky/cuid"
 
@@ -73,28 +70,6 @@ func CreateUser(c *fiber.Ctx, email string, username string, role string, image 
 	// Return the existing User
 	return existingUser, nil
 }
-
-
-// validateCheck performs validation on the provided data using the validator library.
-// It returns an error containing validation error messages if validation fails,
-func ValidateCheck(data interface{}) error {
-	// Attempt to validate the provided data using the validator library
-	if err := Validate.Struct(data); err != nil {
-		// If validation fails, store validation error messages
-		var validationErrors []string
-
-		// Loop through each validation error and format error messages
-		for _, err := range err.(validator.ValidationErrors) {
-			validationErrors = append(validationErrors, fmt.Sprintf("%s validation failed for field %s", err.Tag(), err.Field()))
-		}
-
-		// Construct and return an error message containing all validation errors
-		return fmt.Errorf("validation failed: %s", strings.Join(validationErrors, ", "))
-	}
-
-	return nil
-}
-
 
 // UpdateUserField updates a specific field of a user with a new value.
 // It takes the userID, fieldName, and newValue as parameters.
---
+++ api/db/utils.go
@@ -0,0 +1,25 @@
+package db
+import (
+	"strings"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+)
+// validateCheck performs validation on the provided data using the validator library.
+// It returns an error containing validation error messages if validation fails,
+func ValidateCheck(data interface{}) error {
+	// Attempt to validate the provided data using the validator library
+	if err := Validate.Struct(data); err != nil {
+		// If validation fails, store validation error messages
+		var validationErrors []string
+
+		// Loop through each validation error and format error messages
+		for _, err := range err.(validator.ValidationErrors) {
+			validationErrors = append(validationErrors, fmt.Sprintf("%s validation failed for field %s", err.Tag(), err.Field()))
+		}
+
+		// Construct and return an error message containing all validation errors
+		return fmt.Errorf("validation failed: %s", strings.Join(validationErrors, ", "))
+	}
+
+	return nil
+}
