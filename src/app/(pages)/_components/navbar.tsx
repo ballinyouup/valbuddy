398f92e072a0dcafd51b3c22413c81313a1b323b
ballinyouup <bryanhuici@gmail.com>
2023-08-17 13:24:35 -0400

    added theme toggle + page buttons

--- src/app/(pages)/_components/navbar.tsx
+++ src/app/(pages)/_components/navbar.tsx
@@ -8,12 +8,16 @@ import {
 	PopoverTrigger,
 } from "@/components/ui/popover";
 import LogoutButton from "./logout";
+import { ModeToggle } from "./theme-toggle";
 
 export default async function Navbar() {
 	return (
 		<nav className="flex flex-row h-fit p-4 bg-secondary w-full justify-between">
 			<h4>V</h4>
-			<NavProfile />
+			<div className="flex items-center gap-2">
+				<ModeToggle />
+				<NavProfile />
+			</div>
 		</nav>
 	);
 }
@@ -30,12 +34,22 @@ async function NavProfile() {
 	return (
 		<Popover>
 			<PopoverTrigger>
-				<Avatar className="border-[1.5px] border-transparent hover:border-secondary-foreground hover:cursor-pointer">
+				<Avatar>
 					<AvatarImage src={user.image_url} alt={user.username} />
 					<AvatarFallback>{user.username[0]}</AvatarFallback>
 				</Avatar>
 			</PopoverTrigger>
-			<PopoverContent className="flex justify-end">
+			<PopoverContent className="flex flex-col justify-end gap-2">
+				<Link href="/profile">
+					<Button className="w-full" variant={"secondary"}>
+						Profile
+					</Button>
+				</Link>
+				<Link href="/settings">
+					<Button className="w-full" variant={"secondary"}>
+						Settings
+					</Button>
+				</Link>
 				<LogoutButton>Logout</LogoutButton>
 			</PopoverContent>
 		</Popover>
