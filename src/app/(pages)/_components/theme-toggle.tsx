684f97c879a06c536f7627918cf10b3d69599e6f
ballinyouup <bryanhuici@gmail.com>
2023-08-18 18:04:21 -0400

    default variant

--- src/app/(pages)/_components/theme-toggle.tsx
+++ src/app/(pages)/_components/theme-toggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="outline" className="rounded-full h-fit w-fit p-2">
+				<Button variant="default">
 					<Sun className="h-6 w-6 rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0" />
 					<Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
 					<span className="sr-only">Toggle theme</span>
