1cef23f761a8aea59accf4a59f958266e1b5030c
ballinyouup <bryanhuici@gmail.com>
2023-08-18 17:27:12 -0400

    created right and left sidebar

---
+++ src/app/(pages)/_components/sidebar-left.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function SidebarLeft() {
+	return (
+		<div className="w-56 h-[936px] px-8 py-3 bg-zinc-900 flex-col justify-start items-start gap-3 inline-flex">
+			<Button
+				className="text-white text-xl justify-start font-semibold uppercase p-6"
+				variant={"ghost"}
+				asChild
+			>
+				<Link href="/duo">DUO</Link>
+			</Button>
+			<Button
+				className="text-white text-xl justify-start font-semibold uppercase p-6"
+				variant={"ghost"}
+				asChild
+			>
+				<Link href="/scrims">Scrims</Link>
+			</Button>
+			<Button
+				className="text-white text-xl justify-start font-semibold uppercase p-6"
+				variant={"ghost"}
+				asChild
+			>
+				<Link href="/team">Team</Link>
+			</Button>
+			<Button
+				className="text-white text-xl justify-start font-semibold uppercase p-6"
+				variant={"ghost"}
+				asChild
+			>
+				<Link href="/coaches">Coaches</Link>
+			</Button>
+			<Button
+				className="text-white text-xl justify-start font-semibold uppercase p-6"
+				variant={"ghost"}
+				asChild
+			>
+				<Link href="/clips">Clips</Link>
+			</Button>
+			<Button
+				className="text-white text-xl justify-start font-semibold uppercase p-6"
+				variant={"ghost"}
+				asChild
+			>
+				<Link href="/events">Events</Link>
+			</Button>
+		</div>
+	);
+}
---
+++ src/app/(pages)/_components/sidebar-right.tsx
@@ -0,0 +1,167 @@
+export default function SidebarRight() {
+	return (
+		<div className="w-[380px] h-[936px] bg-zinc-900 flex-col justify-start items-center inline-flex">
+			<div className="self-stretch h-[237px] bg-zinc-900 flex-col justify-start items-center flex">
+				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
+					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
+						SCRIMS
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="w-[200px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							TEAM #1
+						</div>
+					</div>
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							7:00PM
+						</div>
+					</div>
+					<div className="w-[57px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">NA</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="w-[200px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							TEAM #2
+						</div>
+					</div>
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							3:00PM
+						</div>
+					</div>
+					<div className="w-[57px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="w-[25px] text-white text-base font-black capitalize">
+							EU
+						</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="w-[200px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							TEAM #3
+						</div>
+					</div>
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							11:00PM
+						</div>
+					</div>
+					<div className="w-[57px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">AP</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="w-[200px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							TEAM #4
+						</div>
+					</div>
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							9:00PM
+						</div>
+					</div>
+					<div className="w-[57px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">LA</div>
+					</div>
+				</div>
+			</div>
+			<div className="self-stretch h-[237px] bg-zinc-900 flex-col justify-start items-center flex">
+				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
+					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
+						EVENTS
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="w-[260px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							EVENT #1
+						</div>
+					</div>
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">NA</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="w-[260px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							EVENT #2
+						</div>
+					</div>
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">EU</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="w-[260px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							EVENT #3
+						</div>
+					</div>
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							APAC
+						</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="w-[260px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							EVENT #4
+						</div>
+					</div>
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							LATAM
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="self-stretch h-[237px] bg-zinc-900 flex-col justify-start items-center flex">
+				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
+					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
+						COMMUNITIES
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							COMMUNITY #1
+						</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							COMMUNITY #2
+						</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							COMMUNITY #3
+						</div>
+					</div>
+				</div>
+				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
+					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+						<div className="text-white text-base font-black capitalize">
+							COMMUNITY #4
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="w-[380px] h-[67px] py-2 bg-neutral-700 justify-center items-center gap-4 inline-flex">
+				<div className="w-8 h-8 relative" />
+				<div className="text-white text-2xl font-black capitalize">
+					MESSAGES
+				</div>
+			</div>
+		</div>
+	);
+}
