aeb110b83ee70850e2a255d1332924e098f726c1
ballinyouup <bryanhuici@gmail.com>
2023-08-19 23:09:04 -0400

    styling + removed custom width

--- src/app/(pages)/_components/sidebar-right.tsx
+++ src/app/(pages)/_components/sidebar-right.tsx
@@ -1,14 +1,14 @@
 export default function SidebarRight() {
 	return (
-		<div className="w-[380px] h-[936px] bg-zinc-900 flex-col justify-start items-center hidden 2xl:inline-flex">
-			<div className="self-stretch h-[237px] bg-zinc-900 flex-col justify-start items-center flex">
+		<div className="w-96 h-[936px] bg-zinc-900 flex-col justify-start items-center hidden 2xl:inline-flex border-2 border-black">
+			<div className="self-stretch h-56 bg-zinc-900 flex-col justify-start items-center flex">
 				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
 					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
 						SCRIMS
 					</div>
 				</div>
 				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-[200px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+					<div className="w-52 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
 						<div className="text-white text-base font-black capitalize">
 							TEAM #1
 						</div>
@@ -18,12 +18,14 @@ export default function SidebarRight() {
 							7:00PM
 						</div>
 					</div>
-					<div className="w-[57px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">NA</div>
+					<div className="w-16 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							NA
+						</div>
 					</div>
 				</div>
 				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-[200px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+					<div className="w-52 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
 						<div className="text-white text-base font-black capitalize">
 							TEAM #2
 						</div>
@@ -33,14 +35,14 @@ export default function SidebarRight() {
 							3:00PM
 						</div>
 					</div>
-					<div className="w-[57px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="w-[25px] text-white text-base font-black capitalize">
+					<div className="w-16 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="w-6 text-white text-base font-black capitalize">
 							EU
 						</div>
 					</div>
 				</div>
 				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-[200px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+					<div className="w-52 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
 						<div className="text-white text-base font-black capitalize">
 							TEAM #3
 						</div>
@@ -50,12 +52,14 @@ export default function SidebarRight() {
 							11:00PM
 						</div>
 					</div>
-					<div className="w-[57px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">AP</div>
+					<div className="w-16 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							AP
+						</div>
 					</div>
 				</div>
 				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-[200px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+					<div className="w-52 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
 						<div className="text-white text-base font-black capitalize">
 							TEAM #4
 						</div>
@@ -65,39 +69,45 @@ export default function SidebarRight() {
 							9:00PM
 						</div>
 					</div>
-					<div className="w-[57px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">LA</div>
+					<div className="w-16 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+						<div className="text-white text-base font-black capitalize">
+							LA
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className="self-stretch h-[237px] bg-zinc-900 flex-col justify-start items-center flex">
+			<div className="self-stretch h-56 bg-zinc-900 flex-col justify-start items-center flex">
 				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
 					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
 						EVENTS
 					</div>
 				</div>
 				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-[260px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+					<div className="w-64 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
 						<div className="text-white text-base font-black capitalize">
 							EVENT #1
 						</div>
 					</div>
 					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">NA</div>
+						<div className="text-white text-base font-black capitalize">
+							NA
+						</div>
 					</div>
 				</div>
 				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-[260px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+					<div className="w-64 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
 						<div className="text-white text-base font-black capitalize">
 							EVENT #2
 						</div>
 					</div>
 					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">EU</div>
+						<div className="text-white text-base font-black capitalize">
+							EU
+						</div>
 					</div>
 				</div>
 				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-[260px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+					<div className="w-64 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
 						<div className="text-white text-base font-black capitalize">
 							EVENT #3
 						</div>
@@ -109,7 +119,7 @@ export default function SidebarRight() {
 					</div>
 				</div>
 				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-[260px] self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+					<div className="w-64 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
 						<div className="text-white text-base font-black capitalize">
 							EVENT #4
 						</div>
@@ -121,7 +131,7 @@ export default function SidebarRight() {
 					</div>
 				</div>
 			</div>
-			<div className="self-stretch h-[237px] bg-zinc-900 flex-col justify-start items-center flex">
+			<div className="self-stretch h-60 bg-zinc-900 flex-col justify-start items-center flex">
 				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
 					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
 						COMMUNITIES
@@ -156,12 +166,7 @@ export default function SidebarRight() {
 					</div>
 				</div>
 			</div>
-			<div className="w-[380px] h-[67px] py-2 bg-neutral-700 justify-center items-center gap-4 inline-flex">
-				<div className="w-8 h-8 relative" />
-				<div className="text-white text-2xl font-black capitalize">
-					MESSAGES
-				</div>
-			</div>
+			
 		</div>
 	);
 }
