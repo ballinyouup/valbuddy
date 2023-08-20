1e4a4374cfff7427fa79783ece77f0ff1250e2fa
ballinyouup <bryanhuici@gmail.com>
2023-08-20 19:51:26 -0400

    added accordions to sidebar and messages

--- src/app/(pages)/_components/messages.tsx
+++ src/app/(pages)/_components/messages.tsx
@@ -1,10 +1,27 @@
 import { Icons } from '@/components/ui/icons';
-
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger
+} from '@/components/ui/accordion';
 export default function Messages() {
 	return (
-		<div className="fixed bottom-0 right-0 w-96 h-16 py-2 bg-neutral-700 justify-center items-center gap-4 hover:!cursor-pointer hover:ring ring-transparent hover:ring-black md:flex hidden">
-			<Icons.chatMessage />
-			<span className="font-black text-2xl">MESSAGES</span>
-		</div>
+		<Accordion
+			type="single"
+			collapsible
+			className="fixed bottom-0 right-0 w-96 h-fit py-2 bg-neutral-700 justify-center items-center gap-4 hover:!cursor-pointer hover:ring ring-transparent hover:ring-black md:flex hidden"
+		>
+			<AccordionItem value="messages" className="border-0">
+				<AccordionTrigger
+					className="flex justify-center items-center gap-4"
+					icon={false}
+				>
+					<Icons.chatMessage />
+					<span className="font-black text-2xl">MESSAGES</span>
+				</AccordionTrigger>
+				<AccordionContent className="h-96"></AccordionContent>
+			</AccordionItem>
+		</Accordion>
 	);
 }
--- src/app/(pages)/_components/sidebar-right.tsx
+++ src/app/(pages)/_components/sidebar-right.tsx
@@ -1,172 +1,139 @@
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger
+} from '@/components/ui/accordion';
+
 export default function SidebarRight() {
 	return (
-		<div className="w-96 h-[936px] bg-zinc-900 flex-col justify-start items-center hidden 2xl:inline-flex border-2 border-black">
-			<div className="self-stretch h-56 bg-zinc-900 flex-col justify-start items-center flex">
-				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
-					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
-						SCRIMS
-					</div>
+		<Accordion
+			type="multiple"
+			defaultValue={['scrims', 'communities', 'events']}
+			className="w-96 h-[936px] bg-zinc-900 flex-col justify-start items-center hidden 2xl:inline-flex border-2 border-black"
+		>
+			<Scrims />
+			<Events />
+			<Communities />
+		</Accordion>
+	);
+}
+
+function Scrims() {
+	return (
+		<AccordionItem
+			value="scrims"
+			className="h-fit w-full bg-zinc-900 flex-col justify-start items-center flex"
+		>
+			<AccordionTrigger className="w-full py-2 bg-red-900 justify-between items-center gap-2 flex px-4">
+				<div className="text-center text-white text-2xl font-black uppercase">
+					SCRIMS
 				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-52 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							TEAM #1
-						</div>
-					</div>
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							7:00PM
-						</div>
-					</div>
-					<div className="w-16 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							NA
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-52 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							TEAM #2
-						</div>
-					</div>
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							3:00PM
-						</div>
-					</div>
-					<div className="w-16 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="w-6 text-white text-base font-black capitalize">
-							EU
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-52 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							TEAM #3
-						</div>
-					</div>
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							11:00PM
-						</div>
-					</div>
-					<div className="w-16 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							AP
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-52 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							TEAM #4
-						</div>
-					</div>
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							9:00PM
-						</div>
-					</div>
-					<div className="w-16 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							LA
-						</div>
-					</div>
+			</AccordionTrigger>
+			<AccordionContent>
+				<ScrimsRow teamName="Team #1" region="NA" time="7:00PM" />
+				<ScrimsRow teamName="Team #2" region="EU" time="11:00PM" />
+				<ScrimsRow teamName="Team #3" region="AP" time="3:00PM" />
+			</AccordionContent>
+		</AccordionItem>
+	);
+}
+
+function ScrimsRow({
+	teamName,
+	time,
+	region
+}: {
+	teamName: string;
+	time: string;
+	region: string;
+}) {
+	return (
+		<div className="w-full h-fit bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex p-2">
+			<div className="w-48 px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+				<div className="text-white text-base font-black capitalize">
+					{teamName}
 				</div>
 			</div>
-			<div className="self-stretch h-56 bg-zinc-900 flex-col justify-start items-center flex">
-				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
-					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
-						EVENTS
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-64 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							EVENT #1
-						</div>
-					</div>
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							NA
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-64 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							EVENT #2
-						</div>
-					</div>
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							EU
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-64 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							EVENT #3
-						</div>
-					</div>
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							APAC
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="w-64 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							EVENT #4
-						</div>
-					</div>
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
-						<div className="text-white text-base font-black capitalize">
-							LATAM
-						</div>
-					</div>
+			<div className="w-28 px-4 border-r border-black border-opacity-20 justify-start items-center gap-2 flex">
+				<div className="text-white text-base font-black capitalize">
+					{time}
 				</div>
 			</div>
-			<div className="self-stretch h-60 bg-zinc-900 flex-col justify-start items-center flex">
-				<div className="self-stretch py-2 bg-red-900 justify-center items-start gap-2 inline-flex">
-					<div className="grow shrink basis-0 text-center text-white text-2xl font-black uppercase">
-						COMMUNITIES
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							COMMUNITY #1
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							COMMUNITY #2
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							COMMUNITY #3
-						</div>
-					</div>
-				</div>
-				<div className="self-stretch h-12 bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex">
-					<div className="grow shrink basis-0 self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
-						<div className="text-white text-base font-black capitalize">
-							COMMUNITY #4
-						</div>
-					</div>
+			<div className="w-12 self-stretch px-4 justify-start items-center gap-2 flex">
+				<div className="text-white text-base font-black capitalize">
+					{region}
 				</div>
 			</div>
-			
+		</div>
+	);
+}
+
+function Events() {
+	return (
+		<AccordionItem
+			value="events"
+			className="h-fit w-full bg-zinc-900 flex-col justify-start items-center flex"
+		>
+			<AccordionTrigger className="w-full py-2 bg-red-900 justify-between items-center gap-2 flex px-4">
+				<div className="text-center text-white text-2xl font-black uppercase">
+					EVENTS
+				</div>
+			</AccordionTrigger>
+			<AccordionContent>
+				<EventsRow eventName="Event #1" date="01-12-2026" />
+				<EventsRow eventName="Event #2" date="02-28-2026" />
+				<EventsRow eventName="Event #3" date="04-04-2026" />
+			</AccordionContent>
+		</AccordionItem>
+	);
+}
+
+function EventsRow({ eventName, date }: { eventName: string; date: string }) {
+	return (
+		<div className="w-full h-fit bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex p-2">
+			<div className="w-fit grow self-stretch px-4 border-r border-black border-opacity-20 justify-start items-center flex">
+				<div className="text-white text-base font-black capitalize">
+					{eventName}
+				</div>
+			</div>
+			<div className="w-36 px-4 justify-start items-center gap-2 flex">
+				<div className="text-white text-base font-black capitalize">
+					{date}
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function Communities() {
+	return (
+		<AccordionItem
+			value="communities"
+			className="h-fit w-full bg-zinc-900 flex-col justify-start items-center flex"
+		>
+			<AccordionTrigger className="w-full py-2 bg-red-900 justify-between items-center gap-2 flex px-4">
+				<div className="text-center text-white text-2xl font-black uppercase">
+					Communities
+				</div>
+			</AccordionTrigger>
+			<AccordionContent className="w-full">
+				<CommunitiesRow communityName="Team #1" />
+				<CommunitiesRow communityName="Team #2" />
+				<CommunitiesRow communityName="Team #3" />
+			</AccordionContent>
+		</AccordionItem>
+	);
+}
+
+function CommunitiesRow({ communityName }: { communityName: string }) {
+	return (
+		<div className="w-full h-fit bg-neutral-800 border-b border-black border-opacity-30 justify-start items-center inline-flex p-2">
+			<div className="px-4 justify-start items-center flex">
+				<div className="text-white text-base font-black capitalize">
+					{communityName}
+				</div>
+			</div>
 		</div>
 	);
 }
