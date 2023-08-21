a69af134f09d6c525ea807f8eac6336d256f31aa
ballinyouup <bryanhuici@gmail.com>
2023-08-21 00:42:13 -0400

    responsive toggle

--- src/app/(pages)/_components/theme-toggle.tsx
+++ src/app/(pages)/_components/theme-toggle.tsx
@@ -1,16 +1,16 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
+import * as React from 'react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
 
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
 	DropdownMenu,
 	DropdownMenuContent,
 	DropdownMenuItem,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
 
 export function ModeToggle() {
 	const { setTheme } = useTheme();
@@ -18,20 +18,20 @@ export function ModeToggle() {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="default">
+				<Button variant="default" className="hidden md:flex">
 					<Sun className="h-6 w-6 rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0" />
 					<Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
 					<span className="sr-only">Toggle theme</span>
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align="end">
-				<DropdownMenuItem onClick={() => setTheme("light")}>
+				<DropdownMenuItem onClick={() => setTheme('light')}>
 					Light
 				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("dark")}>
+				<DropdownMenuItem onClick={() => setTheme('dark')}>
 					Dark
 				</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme("system")}>
+				<DropdownMenuItem onClick={() => setTheme('system')}>
 					System
 				</DropdownMenuItem>
 			</DropdownMenuContent>
