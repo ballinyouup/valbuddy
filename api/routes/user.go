6a6ce3d807e04b45674043b9c3e8e15b68ed5991
ballinyouup <bryanhuici@gmail.com>
2023-08-30 07:24:33 -0400

    organized user routes and posts

--- api/routes/user.go
+++ api/routes/user.go
@@ -6,7 +6,26 @@ import (
 )
 
 func User(app *fiber.App) {
-	app.Get("/user", handlers.GetUser)
-	app.Post("/user/update", handlers.UpdateUser)
-	app.Delete("/user/delete", handlers.DeleteUser)
+	user := app.Group("/user")
+	user.Get("/", handlers.GetUser)
+	user.Post("/update", handlers.UpdateUser)
+	user.Delete("/delete", handlers.DeleteUser)
+
+	// url/user/account
+	account := user.Group("/account")
+	account.Get("/", handlers.GetAccount)
+	account.Post("/update", handlers.UpdateAccount)
+	account.Delete("/delete", handlers.DeleteAccount)
+
+	// url/user/post
+	post := user.Group("/post")
+	post.Get("/", handlers.GetPost)
+	post.Post("/new", handlers.CreatePost)
+	post.Post("/update", handlers.UpdatePost)
+	post.Delete("/delete", handlers.DeletePost)
+
+	// Get All Posts route
+	app.Get("/posts", handlers.GetPosts)
 }
+
+
