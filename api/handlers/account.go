aef49843de1b79e7608eb1ce7467467ed4d882c8
ballinyouup <bryanhuici@gmail.com>
2023-08-30 07:24:08 -0400

    added placeholder strings to functions

--- api/handlers/account.go
+++ api/handlers/account.go
@@ -1,1 +1,15 @@
 package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+func GetAccount(c *fiber.Ctx) error {
+	return c.SendString("Getting User Account")
+}
+
+func UpdateAccount(c *fiber.Ctx) error {
+	return c.SendString("Updating User Account")
+}
+
+func DeleteAccount(c *fiber.Ctx) error {
+	return c.SendString("User Account Deleted")
+}
--- api/handlers/post.go
+++ api/handlers/post.go
@@ -1,1 +1,23 @@
 package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+func GetPosts(c *fiber.Ctx) error {
+	return c.SendString("Getting All Posts")
+}
+
+func GetPost(c *fiber.Ctx) error {
+	return c.SendString("Getting User Post: 1")
+}
+
+func CreatePost(c *fiber.Ctx) error {
+	return c.SendString("Created User Post: 1")
+}
+
+func UpdatePost(c *fiber.Ctx) error {
+	return c.SendString("Updating User Post: 1")
+}
+
+func DeletePost(c *fiber.Ctx) error {
+	return c.SendString("Deleting User Post: 1")
+}
