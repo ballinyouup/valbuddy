5d2b47e45d21a4811e04c542c7c98d90b7eb8885
ballinyouup <bryanhuici@gmail.com>
2023-08-30 04:30:34 -0400

    removed right sidebar + formatting

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,18 +1,18 @@
-import '../globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import Providers from '@/app/(pages)/_components/providers';
-import Navbar from '@/app/(pages)/_components/navbar';
-import SidebarLeft from './_components/sidebar-left';
-import SidebarRight from './_components/sidebar-right';
-import Messages from './_components/messages';
-import MobileNav from './_components/mobile-nav';
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Providers from "@/app/(pages)/_components/providers";
+import Navbar from "@/app/(pages)/_components/navbar";
+import SidebarLeft from "./_components/sidebar-left";
+import SidebarRight from "./_components/sidebar-right";
+import Messages from "./_components/messages";
+import MobileNav from "./_components/mobile-nav";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: 'VALBuddy',
-	description: 'Find Val Friends'
+	title: "VALBuddy",
+	description: "Find Val Friends"
 };
 
 export default function RootLayout({
@@ -28,7 +28,6 @@ export default function RootLayout({
 					<div className="flex w-full h-full items-start justify-center">
 						<SidebarLeft />
 						{children}
-						<SidebarRight />
 						<Messages />
 						<MobileNav />
 					</div>
