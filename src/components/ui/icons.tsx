abcdb3e4ac0e13677c196b77fee85425071722c7
ballinyouup <bryanhuici@gmail.com>
2023-08-30 04:32:13 -0400

    added jsx names to svg

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -228,9 +228,9 @@ export const Icons = {
 			viewBox="0 0 24 24"
 			fill="none"
 			stroke="currentColor"
-			stroke-width="3"
-			stroke-linecap="round"
-			stroke-linejoin="round"
+			strokeWidth="3"
+			strokeLinecap="round"
+			strokeLinejoin="round"
 			className="lucide lucide-message-square"
 			{...props}
 		>
