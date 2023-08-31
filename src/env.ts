4e22bf09cc30cdaa177513ef2de873f1f0ae65f0
ballinyouup <bryanhuici@gmail.com>
2023-08-31 15:08:09 -0400

    added frontend url

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,4 @@
 export const config = {
     API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000/api" : process.env.NEXT_PUBLIC_API_URL as string,
-}
+    FRONTEND_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.FRONTEND_URL as string,
+};
