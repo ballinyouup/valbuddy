0fcd095ce6d6946166d40fed32a30e71a3f764a6
ballinyouup <bryanhuici@gmail.com>
2023-09-07 00:01:15 -0400

    added plus icon

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -236,5 +236,23 @@ export const Icons = {
 		>
 			<path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
 		</svg>
+	),
+	plus: (props: IconProps) => (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			className="lucide lucide-plus"
+			{...props}
+		>
+			<path d="M5 12h14" />
+			<path d="M12 5v14" />
+		</svg>
 	)
 };
