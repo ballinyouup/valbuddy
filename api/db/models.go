7b76c6aff1098a8e465421e51bd6d886fb3bb15c
ballinyouup <bryanhuici@gmail.com>
2023-09-06 23:53:10 -0400

    added more fields to post model

--- api/db/models.go
+++ api/db/models.go
@@ -6,7 +6,8 @@ import (
 
 // Sensitive User Data
 type User struct {
-	ID       string `json:"id" gorm:"primaryKey" validate:"required,min=1"`   // User ID
+	ID string `json:"id" gorm:"primaryKey" validate:"required,min=1"` // User ID
+
 	Email    string `json:"email" gorm:"unique" validate:"required,email"`    // OAuth Email
 	Username string `json:"username" gorm:"unique" validate:"required,min=1"` // OAuth Username/VALBuddy Username
 	Image    string `json:"image_url" validate:"required,min=1"`              // OAuth Image
@@ -18,11 +19,13 @@ type User struct {
 
 // VALBuddy Data
 type Account struct {
-	ID       string `json:"id" gorm:"primaryKey" validate:"required,min=1"` // Account ID
-	UserID   string `json:"user_id" validate:"required,min=1"`              // ID from User Model
-	Username string `json:"username" validate:"omitempty"`                  // VALORANT Username
-	Rank     string `json:"rank" validate:"omitempty"`                      // VALORANT RANK
-	Role     string `json:"role" validate:"required"` // Role FREE or PAID
+	ID     string `json:"id" gorm:"primaryKey" validate:"required,min=1"` // Account ID
+	UserID string `json:"user_id" validate:"required,min=1"`              // ID from User Model
+
+	Username string `json:"username" validate:"omitempty"` // VALBuddy Username
+	Rank     string `json:"rank" validate:"omitempty"`     // VALORANT RANK
+	Role     string `json:"role" validate:"required"`      // Role FREE or PAID
+	Region   string `json:"region" validate:"required"`    // NA/EU etc...
 
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
@@ -31,12 +34,15 @@ type Account struct {
 type Post struct {
 	ID       string `json:"id" gorm:"primaryKey" validate:"required,min=1"` // Post ID
 	UserID   string `json:"user_id" validate:"required,min=1"`              // ID from User Model
-	Username string `json:"username" validate:"omitempty"`                  // Username from Account Model
+	Username string `json:"username" validate:"omitempty"`                  // VALORANT Username
 
-	Text        string    `json:"text" gorm:"text" validate:"required,min=1"`    // Post Text
-	Players     int       `json:"player_amount" validate:"required,min=0,max=5"` // VALORANT PLAYER #: 0<=x<=5
-	PlayerRoles []string  `json:"player_roles" gorm:"type:json"`                 // VALORANT ROLES
-	PlayerRanks []string  `json:"player_ranks" gorm:"type:json"`                 // VALORANT RANK/RANGE FILTER
-	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+	Region      string `json:"region" validate:"required"`                    // NA/EU etc...
+	Category    string `json:"category" validate:"required,min=1"`            // Category: Duos, Team, etc.
+	Text        string `json:"text" gorm:"text" validate:"required,min=1"`    // Post Text
+	Players     int    `json:"player_amount" validate:"required,min=1,max=5"` // VALORANT PLAYER #: 1<=x<=5
+	PlayerRoles []byte `json:"player_roles" gorm:"type:json"`                 // VALORANT ROLES
+	PlayerRanks []byte `json:"player_ranks" gorm:"type:json"`                 // VALORANT RANK/RANGE FILTER
+
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
