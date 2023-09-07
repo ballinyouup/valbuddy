e0188709b7a1ac9127b2b54f97a7895e47858c24
ballinyouup <bryanhuici@gmail.com>
2023-09-06 23:56:40 -0400

    added Todo

--- api/handlers/account.go
+++ api/handlers/account.go
@@ -1,7 +1,7 @@
 package handlers
 
 import "github.com/gofiber/fiber/v2"
-
+//TODO: Finish Account CRUD Functions
 func GetAccount(c *fiber.Ctx) error {
 	return c.SendString("Getting User Account")
 }
