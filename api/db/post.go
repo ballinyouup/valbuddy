abab921a7b1dae690919dcf0dd56a3b3a7bb70c6
ballinyouup <bryanhuici@gmail.com>
2023-09-06 23:54:07 -0400

    created CRUD in db/handlers server action+zod

--- api/db/post.go
+++ api/db/post.go
@@ -1,1 +1,87 @@
 package db
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/lucsky/cuid"
+)
+
+func GetPosts() ([]Post, error) {
+	var posts []Post
+	if err := GetDatabase().Find(&posts).Error; err != nil {
+		return posts, fmt.Errorf("error fetching all posts: %w", err)
+	}
+	return posts, nil
+}
+
+func GetUserPosts(id string, limit int) ([]Post, error) {
+	var posts []Post
+	searchQuery := &Post{
+		UserID: id,
+	}
+	if err := GetDatabase().Limit(limit).Where(searchQuery).Find(&posts).Error; err != nil {
+		return posts, fmt.Errorf("error fetching post: %w", err)
+	}
+	return posts, nil
+}
+
+func CreatePost(UserID string, Text string, Username string, Region string, Category string, PlayerAmount int, PlayerRoles []string, PlayerRanks []string) error {
+	var account Account
+	query := Account{
+		ID: UserID,
+	}
+	if err := GetDatabase().Where(query).Find(&account).Error; err != nil {
+		return fmt.Errorf("error finding user in create post: %w", err)
+	}
+	roles, err := json.Marshal(PlayerRoles)
+	if err != nil {
+		return fmt.Errorf("error marshaling PlayerRoles: %w", err)
+	}
+	ranks, err := json.Marshal(PlayerRanks)
+	if err != nil {
+		return fmt.Errorf("error marshaling PlayerRoles: %w", err)
+	}
+	post := &Post{
+		ID:          cuid.New(),
+		UserID:      UserID,
+		Username:    Username,
+		Category:    Category,
+		Text:        Text,
+		Players:     PlayerAmount,
+		PlayerRoles: roles,
+		PlayerRanks: ranks,
+		Region:      Region,
+	}
+	log.Print(post)
+	if err := GetDatabase().Create(post).Error; err != nil {
+		return fmt.Errorf("error creating post: %w", err)
+	}
+	return nil
+}
+
+func UpdatePost(userID string, postID string, text string, playerAmount int, playerRoles []string, playerRanks []string) error {
+	// Create a map to store the fields you want to update
+	updates := map[string]interface{}{
+		"Text":        text,
+		"Players":     playerAmount,
+		"PlayerRoles": playerRoles,
+		"PlayerRanks": playerRanks,
+	}
+
+	query := &Post{
+		ID:     postID,
+		UserID: userID,
+	}
+	// Update the post with the given ID
+	if err := GetDatabase().Model(&Post{}).Where(query).Updates(updates).Error; err != nil {
+		return fmt.Errorf("error updating post: %w", err)
+	}
+
+	return nil
+}
+
+func DeletePost() error {
+	return nil
+}
--- api/handlers/post.go
+++ api/handlers/post.go
@@ -1,21 +1,87 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"nextjs-go/db"
+	"strconv"
+	"strings"
 
+	"github.com/gofiber/fiber/v2"
+)
+
+// TODO: Finish Post CRUD Functions
 func GetPosts(c *fiber.Ctx) error {
-	return c.SendString("Getting All Posts")
+	posts, err := db.GetPosts()
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("error executing get posts: %s", err))
+	}
+	return c.JSON(posts)
 }
 
-func GetPost(c *fiber.Ctx) error {
-	return c.SendString("Getting User Post: 1")
+func GetUserPosts(c *fiber.Ctx) error {
+	s, err := db.ValidateSession(c)
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnauthorized, fmt.Sprintf("error validating session %s", err))
+	}
+
+	limit, err := strconv.Atoi(c.Params("limit"))
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("error converting string %s", err))
+	}
+
+	// Retrieve the user ID from the session
+	userId := s.Get("user_id").(string)
+	posts, err := db.GetUserPosts(userId, limit)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("error executing get posts: %s", err))
+	}
+	// Return the user data as JSON response
+	return c.JSON(posts)
+}
+
+type Post struct {
+	Username string   `json:"username"`
+	Category string   `json:"category"`
+	Text     string   `json:"text"`
+	Amount   int      `json:"amount"`
+	Roles    []string `json:"roles" gorm:"type:json"`
+	Ranks    []string `json:"ranks" gorm:"type:json"`
+	Region   string   `json:"region"`
 }
 
 func CreatePost(c *fiber.Ctx) error {
-	return c.SendString("Created User Post: 1")
+	s, err := db.ValidateSession(c)
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnauthorized, fmt.Sprintf("error validating session %s", err))
+	}
+	userId := s.Get("user_id").(string)
+	var post Post
+	if err := c.BodyParser(&post); err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("error parsing body: %s", err))
+	}
+	if err := db.CreatePost(userId, post.Text, post.Username, post.Region, post.Category, post.Amount, post.Roles, post.Ranks); err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("error executing create posts: %s", err))
+	}
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func UpdatePost(c *fiber.Ctx) error {
-	return c.SendString("Updating User Post: 1")
+	s, err := db.ValidateSession(c)
+	if err != nil {
+		return fiber.NewError(fiber.StatusUnauthorized, fmt.Sprintf("error validating session %s", err))
+	}
+	userID := s.Get("user_id").(string)
+	postID := c.FormValue("PostID")
+	playerAmount, err := strconv.Atoi(c.FormValue("PlayerAmount"))
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("error converting string: %s", err))
+	}
+	playerRoles := strings.Split(c.FormValue("PlayerRoles"), ",")
+	playerRanks := strings.Split(c.FormValue("PlayerRanks"), ",")
+	if err := db.UpdatePost(userID, postID, c.FormValue("Text"), playerAmount, playerRoles, playerRanks); err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, fmt.Sprintf("error executing get posts: %s", err))
+	}
+	return c.SendStatus(fiber.StatusOK)
 }
 
 func DeletePost(c *fiber.Ctx) error {
