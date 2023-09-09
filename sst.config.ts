6910365d52dc8385d476c87e5d2dd9f14749600c
ballinyouup <bryanhuici@gmail.com>
2023-09-08 22:17:49 -0400

    changed region to us-east-2

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,16 @@
 import { SSTConfig } from "sst";
 import Site from "./stacks/site";
 import Backend from "./stacks/backend";
+
 export default {
   config(_input) {
     return {
       name: "nextjs-go",
-      region: "us-east-1",
+      region: "us-east-2",
     };
   },
   stacks(app) {
-    app.stack(Site)
+    app.stack(Site);
     app.stack(Backend)
   },
 } satisfies SSTConfig;
