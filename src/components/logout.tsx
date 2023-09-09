b55123e1ee50ccbb929caa90187aabd1c525156f
ballinyouup <bryanhuici@gmail.com>
2023-09-08 22:31:10 -0400

    based on current path, it will refresh or go home

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -1,27 +1,42 @@
 "use client";
 import { config } from "@/env";
 import { Button } from "./ui/button";
-import { useRouter } from "next/navigation";
-
+import { useRouter, usePathname } from "next/navigation";
+import { toast } from "./ui/use-toast";
 export default function LogoutButton({
 	children
 }: {
 	children?: React.ReactNode;
 }) {
 	const router = useRouter();
-	async function Logout() {
-		const data = await fetch(`${config.API_URL}/logout`, {
-			credentials: "include",
-			method: "GET"
-		});
-		if (data.ok) {
-			router.push("/");
+	const currentPath = usePathname();
+	async function Logout(currentPath: string) {
+		try {
+			const data = await fetch(`${config.API_URL}/logout`, {
+				credentials: "include",
+				method: "GET",
+				cache: "no-store",
+				mode: "no-cors"
+			});
+			if (!data.ok) {
+				throw new Error("Error logging out");
+			}
+			currentPath === "/"
+				? router.refresh()
+				: router.push("/");
+				
+		} catch (error) {
+			toast({
+				title: "Error signing out",
+				description: JSON.stringify(error),
+				variant: "destructive"
+			});
 		}
 	}
 
 	return (
 		<Button
-			onClick={() => Logout()}
+			onClick={() => Logout(currentPath)}
 			className="w-full rounded-none font-black text-lg uppercase tracking-wide"
 		>
 			{children}
