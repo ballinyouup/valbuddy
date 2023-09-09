54cb02fe274a08d7ce896254d7bc3346a5fc4350
ballinyouup <bryanhuici@gmail.com>
2023-09-09 01:56:29 -0400

    put the fetch inside comp/ individ comp

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,4 +1,4 @@
-import { GetAllPosts, type Post } from "@/api/post";
+import { GetDuosPosts, GetTeamsPosts, type Post } from "@/api/post";
 import SidebarRight from "./_components/sidebar-right";
 import {
 	Accordion,
@@ -7,8 +7,6 @@ import {
 	AccordionTrigger
 } from "@/components/ui/accordion";
 export default async function Home() {
-	const posts = await GetAllPosts();
-	if (!posts) return null;
 	return (
 		<>
 			<main className="h-full w-full">
@@ -26,23 +24,7 @@ export default async function Home() {
 							</span>
 						</AccordionTrigger>
 						<AccordionContent>
-							<div className="flex flex-col items-center justify-center">
-								{posts.map(
-									(
-										post,
-										index
-									) => (
-										<UserRow
-											key={String(
-												index
-											)}
-											post={
-												post
-											}
-										/>
-									)
-								)}
-							</div>
+							<Duos />
 						</AccordionContent>
 					</AccordionItem>
 					<AccordionItem
@@ -57,13 +39,9 @@ export default async function Home() {
 								TEAMS
 							</span>
 						</AccordionTrigger>
-						{/**<AccordionContent>
-							<div className="flex flex-col items-center justify-center">
-								<UserRow
-									{...posts[0]}
-								/>
-							</div>
-										</AccordionContent> */}
+						<AccordionContent>
+							<Teams />
+						</AccordionContent>
 					</AccordionItem>
 				</Accordion>
 			</main>
@@ -72,7 +50,19 @@ export default async function Home() {
 	);
 }
 
-function UserRow({ post, key }: { post: Post; key: string }) {
+async function Duos() {
+	const posts = await GetDuosPosts();
+	if (!posts) return <>No posts T_T </>;
+	return (
+		<div className="flex flex-col items-center justify-center">
+			{posts.map((post) => (
+				<DuosRow key={post.id} post={post} />
+			))}
+		</div>
+	);
+}
+
+function DuosRow({ post, key }: { post: Post; key: string }) {
 	const roles = JSON.parse(
 		Buffer.from(post.player_roles, "base64").toString("utf-8")
 	);
@@ -81,7 +71,94 @@ function UserRow({ post, key }: { post: Post; key: string }) {
 	);
 
 	return (
-		<div key={key} className="flex h-fit w-full max-w-7xl items-center border-black border-opacity-30 bg-neutral-800">
+		<div
+			key={key}
+			className="flex h-fit w-full max-w-7xl items-center border-black border-opacity-30 bg-neutral-800"
+		>
+			<div className="p-2">
+				<div className="h-20 w-20 bg-white" />
+			</div>
+			<div className="flex flex-col w-full justify-evenly px-4 text-primary-foreground font-medium text-base">
+				<div className="flex justify-between uppercase">
+					<span>{post.username}</span>
+					<div className="flex gap-2">
+						{ranks.map(
+							(
+								rank: string,
+								index: number
+							) => (
+								<span
+									key={
+										index
+									}
+								>
+									{rank}
+								</span>
+							)
+						)}
+					</div>
+				</div>
+				<div className="flex justify-between uppercase">
+					<span>{post.region}</span>
+					<span>{post.player_amount}</span>
+				</div>
+				<div className="flex justify-between">
+					<span className="uppercase">
+						{roles.map(
+							(
+								role: string,
+								index: number
+							) => (
+								<span
+									key={
+										index
+									}
+								>
+									{role}
+								</span>
+							)
+						)}
+					</span>
+					<span>
+						{post.created_at?.toLocaleString()}
+					</span>
+				</div>
+				<div className="flex justify-between">
+					<span className="uppercase">
+						{post.text}
+					</span>
+					<span>{post.category}</span>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+async function Teams() {
+	const posts = await GetTeamsPosts();
+	if (!posts) return <>No posts T_T </>;
+	return (
+		<div className="flex flex-col items-center justify-center">
+			{posts.map((post) => (
+				<TeamsRow key={post.id} post={post} />
+			))}
+		</div>
+	);
+}
+
+function TeamsRow({ post, key }: { post: Post; key: string }) {
+	const roles = JSON.parse(
+		Buffer.from(post.player_roles, "base64").toString("utf-8")
+	);
+	const ranks = JSON.parse(
+		Buffer.from(post.player_ranks, "base64").toString("utf-8")
+	);
+
+	return (
+		<div
+			key={key}
+			className="flex h-fit w-full max-w-7xl items-center border-black border-opacity-30 bg-neutral-800"
+		>
 			<div className="p-2">
 				<div className="h-20 w-20 bg-white" />
 			</div>
