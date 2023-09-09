fc477a3ca3e7918eba1899104e64168240f066b6
ballinyouup <bryanhuici@gmail.com>
2023-09-08 22:34:09 -0400

    added custom domain/stack type

--- stacks/site.ts
+++ stacks/site.ts
@@ -1,9 +1,14 @@
-import { NextjsSite } from "sst/constructs";
+import { NextjsSite, StackContext } from "sst/constructs";
 
-export default function Site({ stack }: any) {
+export default function Site({ stack }: StackContext) {
     const site = new NextjsSite(stack, "site", {
         environment: {
             NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL as string
+        },
+        customDomain: {
+            domainName: "valbuddy.com",
+            domainAlias: "www.valbuddy.com",
+            hostedZone: "valbuddy.com", 
         }
     });
     stack.addOutputs({
