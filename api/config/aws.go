09c5211f3befb651b556f64fa7d31796ee180d75
ballinyouup <bryanhuici@gmail.com>
2023-09-09 04:04:31 -0400

    fixed file uploads to img cdn+ delete file fn

--- api/config/aws.go
+++ api/config/aws.go
@@ -28,8 +28,8 @@ func AWSInit() error {
 }
 
 func UploadFile(file io.Reader, fileName string) (string, error) {
-	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-		Bucket:      aws.String("valbuddy-images"),
+	_, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
+		Bucket:      aws.String("img.valbuddy.com"),
 		Key:         aws.String(fileName),
 		Body:        file,
 		ContentType: aws.String("image/jpeg"),
@@ -38,5 +38,19 @@ func UploadFile(file io.Reader, fileName string) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("error uploading file: %w", err)
 	}
-	return result.Location, nil
+
+	return fmt.Sprintf("https://img.valbuddy.com/%s", fileName), nil
+}
+
+func DeleteFile(fileName string) error {
+	_, err := client.DeleteObject(context.TODO(), &s3.DeleteObjectInput{
+		Bucket: aws.String("img.valbuddy.com"),
+		Key:    aws.String(fileName),
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to delete file %s, %v", fileName, err)
+	}
+
+	return nil
 }
