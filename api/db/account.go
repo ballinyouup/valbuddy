980ccc27506fa53de983c9c97a6db646642990d7
ballinyouup <bryanhuici@gmail.com>
2023-09-10 19:45:33 -0400

    created get account function for user account

--- api/db/account.go
+++ api/db/account.go
@@ -1,1 +1,18 @@
 package db
+
+import (
+	"fmt"
+)
+
+func GetAccount(userID string)(Account, error) {
+	// Create an empty User object
+	var account Account
+	query := Account{
+		UserID: userID,
+	}
+	// Retrieve account data from the database based on the user ID
+	if err := GetDatabase().Omit("email", "username").Where(query).First(&account).Error; err != nil {
+		return Account{}, fmt.Errorf("error getting user: %w", err)
+	}
+	return account, nil
+}
