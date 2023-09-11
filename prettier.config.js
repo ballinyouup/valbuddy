b505f630672adc7bdb9e5009cf8f16be97d9a855
ballinyouup <bryanhuici@gmail.com>
2023-09-11 03:20:02 -0400

    added formatting from prettierrc

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     plugins: ['prettier-plugin-tailwindcss'],
-}
+    "singleQuote": false,
+    "editor.formatOnSave": true,
+    "tabWidth": 4,
+    "requireConfig": false,
+    "trailingComma": "none",
+    "bracketSpacing": false,
+    "jsxBracketSameLine": false,
+    "semi": true
+};
