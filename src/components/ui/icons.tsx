e91e3133bce1508524f4d6706f8d14a1b2a774af
ballinyouup <bryanhuici@gmail.com>
2023-09-11 03:26:17 -0400

    new info icon

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -254,5 +254,24 @@ export const Icons = {
 			<path d="M5 12h14" />
 			<path d="M12 5v14" />
 		</svg>
+	),
+	info: (props: IconProps) => (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			className="lucide lucide-info"
+			{...props}
+		>
+			<circle cx="12" cy="12" r="10" />
+			<path d="M12 16v-4" />
+			<path d="M12 8h.01" />
+		</svg>
 	)
 };
