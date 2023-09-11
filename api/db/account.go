de4b64eae26285aaf8c1ffcad6adb236ad12c949
ballinyouup <bryanhuici@gmail.com>
2023-09-11 03:20:24 -0400

    removed omit from account

--- api/db/account.go
+++ api/db/account.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 )
 
-func GetAccount(userID string)(Account, error) {
+func GetAccount(userID string) (Account, error) {
 	// Create an empty User object
 	var account Account
 	query := Account{
 		UserID: userID,
 	}
 	// Retrieve account data from the database based on the user ID
-	if err := GetDatabase().Omit("email", "username").Where(query).First(&account).Error; err != nil {
+	if err := GetDatabase().Where(query).First(&account).Error; err != nil {
 		return Account{}, fmt.Errorf("error getting user: %w", err)
 	}
 	return account, nil
