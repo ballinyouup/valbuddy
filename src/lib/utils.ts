f02d87cba486ef4829f2a050a4b5722d3d8c35c9
ballinyouup <bryanhuici@gmail.com>
2023-09-16 05:14:24 -0400

    parseJSON function with err handling

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function parseJSON(json: string) {
+  let j;
+  try {
+    j = JSON.parse(atob(json));
+  } catch {
+    j = [];
+  }
+  return j;
+}
