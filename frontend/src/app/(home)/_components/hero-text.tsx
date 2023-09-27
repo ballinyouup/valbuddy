78514c72fea0a4b621bb1a378eaa47e5f7f7f143
ballinyouup <bryanhuici@gmail.com>
2023-09-27 05:27:08 -0400

    completed 2nd feature section + added images

---
+++ frontend/src/app/(home)/_components/hero-text.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState, useEffect } from "react";
+export default function HeroText() {
+    const words = ["MAKE FRIENDS", "CREATE TEAMS", "JOIN EVENTS"];
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % words.length);
+        }, 2500);
+
+        // Clean up interval on unmount
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <AnimatePresence mode="wait">
+            <motion.h2
+                key={words[index].split(" ")[0]}
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, x: 50 }}
+                transition={{ duration: 0.5 }}
+                className="flex flex-col text-[16vw] sm:text-8xl"
+            >
+                <span className="text-primary">
+                    {words[index].split(" ")[0]}
+                </span>
+            </motion.h2>
+            <motion.h2
+                key={words[index].split(" ")[1]}
+                initial={{ opacity: 0, y: -30 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, x: 50 }}
+                transition={{ delay: 0.2, duration: 0.4 }}
+                className="flex flex-col text-[16vw] sm:text-8xl"
+            >
+                {words[index].split(" ")[1]}
+            </motion.h2>
+        </AnimatePresence>
+    );
+}
--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -4,10 +4,11 @@ import { Icons } from "@/components/ui/icons";
 import Messages from "./_components/messages";
 import InView from "./_components/in-view";
 import { Separator } from "@/components/ui/separator";
-
+import HeroText from "./_components/hero-text";
+import { Verified } from "lucide-react";
 export default function Page() {
     return (
-        <div className="flex flex-col">
+        <div className="flex h-full flex-col">
             <Hero />
             <Features />
         </div>
@@ -16,25 +17,11 @@ export default function Page() {
 
 function Hero() {
     return (
-        <div className="flex w-screen lg:w-full">
+        <div className="flex h-full w-screen lg:w-full">
             <div className="LeftHero flex h-full w-full items-center justify-center bg-gradient-to-tr from-transparent via-transparent to-red-950/90 px-8 lg:h-desktop lg:w-1/2 lg:px-10">
-                <div className="HeroTextContainer flex h-full w-fit flex-col justify-between gap-10 pt-40 lg:h-3/5 lg:w-full lg:py-0">
-                    <div className="HeroTextHeaders flex w-full flex-col lg:whitespace-nowrap [&>h2]:text-[10vw] [&>h2]:min-[540px]:text-6xl [&>h2]:sm:text-6xl [&>h2]:md:text-7xl [&>h2]:lg:text-6xl [&>h2]:xl:text-7xl">
-                        <h2 className="font-black uppercase">
-                            <span className="text-primary">Make</span> Friends
-                        </h2>
-                        <h2 className="font-black uppercase">
-                            Create <span className="text-primary">Teams</span>
-                        </h2>
-                        <h2 className="flex flex-col font-black uppercase leading-[1.1]">
-                            <div className="flex flex-wrap">
-                                <span className="text-primary underline">
-                                    Dominate&nbsp;
-                                </span>
-                                <span>The</span>
-                            </div>
-                            <span>Competition</span>
-                        </h2>
+                <div className="HeroTextContainer flex h-screen w-fit flex-col items-center justify-center gap-10 lg:h-3/5 lg:w-full lg:py-0">
+                    <div className="HeroTextHeaders flex w-full flex-col lg:whitespace-nowrap">
+                        <HeroText />
                     </div>
                     <div className="HeroParagraphText max-w-3xl">
                         <p className="text-base sm:text-lg lg:text-xl">
@@ -69,6 +56,8 @@ function Hero() {
                     width={895}
                     alt="VALORANT Hero Image"
                     className="object-cover mix-blend-soft-light"
+                    priority
+                    quality={100}
                 />
             </div>
         </div>
@@ -98,7 +87,7 @@ function Features() {
 function FeatureTitle() {
     return (
         <div className="flex w-full max-w-6xl flex-col self-center">
-            <div className="flex w-full flex-wrap justify-center px-8 py-16 text-4xl font-black uppercase text-white md:text-5xl lg:py-20 lg:text-6xl">
+            <div className="flex w-full flex-wrap justify-center px-8 pb-16 text-4xl font-black uppercase text-white md:text-5xl lg:py-20 lg:text-6xl">
                 <span>Find the &nbsp;</span>
                 <span className="text-red-700">Perfect&nbsp;</span>
                 <span>Teammates</span>
@@ -111,10 +100,10 @@ function FeatureTitle() {
 function FeatureOne() {
     return (
         <div className="flex w-full flex-col-reverse flex-wrap items-center justify-center gap-20 lg:flex-row lg:gap-0 lg:py-32">
-            <div className="flex w-full max-w-xl items-center justify-center lg:w-1/2">
+            <div className="flex w-full max-w-2xl items-center justify-center lg:w-1/2">
                 <Messages />
             </div>
-            <div className="flex w-full max-w-xl items-center justify-center lg:w-1/2">
+            <div className="flex w-full max-w-2xl items-center justify-center lg:w-1/2">
                 <div className="flex h-full w-full flex-col items-center justify-center gap-4 lg:w-2/3">
                     <div className="flex w-full flex-col items-center gap-8 sm:flex-row">
                         <div className="flex items-center justify-center sm:w-1/3">
@@ -142,7 +131,7 @@ function FeatureOne() {
 function FeatureTwo() {
     return (
         <div className="flex w-full flex-col items-center justify-center gap-20 lg:flex-row lg:gap-0 lg:py-32">
-            <div className="flex w-full max-w-xl items-center justify-center lg:w-1/2">
+            <div className="flex w-full max-w-2xl items-center justify-center lg:w-1/2">
                 <div className="flex h-full w-full flex-col items-center justify-center gap-4 lg:w-2/3">
                     <div className="flex w-full flex-col items-center gap-8 sm:flex-row">
                         <div className="flex items-center justify-center sm:w-1/3">
@@ -164,8 +153,116 @@ function FeatureTwo() {
                     </p>
                 </div>
             </div>
-            <div className="flex w-full max-w-xl items-center justify-center lg:w-1/2">
-                <div className="flex h-80 w-full flex-col overflow-hidden rounded-3xl bg-white shadow-[0_25px_175px_-12px] shadow-red-900/90 lg:w-2/3" />
+            <div className="flex w-full max-w-2xl items-center justify-center lg:w-1/2">
+                <div className="flex h-80 w-full flex-col overflow-hidden rounded-3xl shadow-[0_25px_175px_-12px] shadow-red-900/90 lg:w-2/3">
+                    <div className="relative flex gap-4 bg-red-950 p-6">
+                        <Image
+                            src="/jett-portrait.jpg"
+                            alt="Jett Portrait"
+                            height={80}
+                            width={80}
+                            className="rounded-full border-2 border-black"
+                        />
+                        <div className="absolute left-20 top-20 flex h-6 w-6 items-center justify-center overflow-hidden rounded-full bg-black object-cover">
+                            <Image
+                                src={"/rank-ascendant.png"}
+                                width={48}
+                                height={48}
+                                alt="Ascendant Rank"
+                                className="h-10 w-10 object-cover"
+                            />
+                        </div>
+                        <div className="h-full w-full">
+                            <div className="flex justify-between">
+                                <span className="flex items-center gap-2 font-bold">
+                                    JettPlayer256{" "}
+                                    <Verified
+                                        width={24}
+                                        height={24}
+                                        stroke="green"
+                                        fill="black"
+                                    />
+                                </span>
+                                <div className="flex h-5 w-full justify-end gap-1">
+                                    <Image
+                                        src="/role-controller.png"
+                                        alt="Controller Icon"
+                                        height={18}
+                                        width={18}
+                                        className="rounded-full"
+                                    />
+                                    <Image
+                                        src="/role-sentinel.png"
+                                        alt="Sentinel Icon"
+                                        height={18}
+                                        width={18}
+                                        className="rounded-full"
+                                    />
+                                    <Image
+                                        src="/role-initiator.png"
+                                        alt="Initiator Icon"
+                                        height={18}
+                                        width={18}
+                                        className="rounded-full"
+                                    />
+                                    <Image
+                                        src="/role-duelist.png"
+                                        alt="Duelist Icon"
+                                        height={18}
+                                        width={18}
+                                        className="rounded-full"
+                                    />
+                                    <Image
+                                        src="/role-fill.png"
+                                        alt="Fill Icon"
+                                        height={18}
+                                        width={18}
+                                        className="rounded-full"
+                                        quality={100}
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex justify-between">
+                                <span className="font-bold">NA</span>
+                                <div className="flex h-6 gap-1">
+                                    <Image
+                                        src="/jett-portrait.jpg"
+                                        alt="Initiator Icon"
+                                        height={24}
+                                        width={24}
+                                        className="rounded-full"
+                                        quality={100}
+                                    />
+                                    <Image
+                                        src="/agent-raze.png"
+                                        alt="Duelist Icon"
+                                        height={24}
+                                        width={24}
+                                        className="rounded-full"
+                                        quality={100}
+                                    />
+                                    <Image
+                                        src="/pheonix-portrait.jpg"
+                                        alt="Fill Icon"
+                                        height={24}
+                                        width={24}
+                                        className="rounded-full"
+                                        quality={100}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex h-full flex-col gap-4 p-6">
+                        <h6>Looking For Team </h6>
+                        <p>
+                            Hello everyone, my name is JettPlayer256 and im a
+                            smokes main who has been playing FPS for more than 2
+                            years. I started playing valorant since Beta.
+                            Currently in college, looking for serious players
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     );
@@ -174,10 +271,10 @@ function FeatureTwo() {
 function FeatureThree() {
     return (
         <div className="flex w-full flex-col-reverse items-center justify-center gap-20 lg:flex-row lg:gap-0 lg:py-32">
-            <div className="flex w-full max-w-xl items-center justify-center lg:w-1/2">
+            <div className="flex w-full max-w-2xl items-center justify-center lg:w-1/2">
                 <div className="flex h-80 w-full flex-col overflow-hidden rounded-3xl bg-white shadow-[0_25px_175px_-12px] shadow-red-900/90 lg:w-2/3" />
             </div>
-            <div className="flex w-full max-w-xl items-center justify-center lg:w-1/2">
+            <div className="flex w-full max-w-2xl items-center justify-center lg:w-1/2">
                 <div className="flex h-full w-full flex-col items-center justify-center gap-4 lg:w-2/3">
                     <div className="flex w-full flex-col items-center gap-8 sm:flex-row">
                         <div className="flex items-center justify-center sm:w-1/3">
@@ -205,7 +302,7 @@ function FeatureThree() {
 function FeatureFour() {
     return (
         <div className="flex w-full flex-col items-center justify-center gap-20 lg:flex-row lg:gap-0 lg:py-32">
-            <div className="flex w-full max-w-xl items-center justify-center lg:w-1/2">
+            <div className="flex w-full max-w-2xl items-center justify-center lg:w-1/2">
                 <div className="flex h-full w-full flex-col items-center justify-center gap-4 lg:w-2/3">
                     <div className="flex w-full flex-col items-center gap-8 sm:flex-row">
                         <div className="flex items-center justify-center sm:w-1/3">
@@ -227,7 +324,7 @@ function FeatureFour() {
                     </p>
                 </div>
             </div>
-            <div className="flex w-full max-w-xl items-center justify-center lg:w-1/2">
+            <div className="flex w-full max-w-2xl items-center justify-center lg:w-1/2">
                 <div className="flex h-80 w-full flex-col overflow-hidden rounded-3xl bg-white shadow-[0_25px_175px_-12px] shadow-red-900/90 lg:w-2/3" />
             </div>
         </div>
